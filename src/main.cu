31506d957f935fef08d102062e144fd901d6c5a8
Rayan El Idrissi <150552246+rayan-elidrissi@users.noreply.github.com>
2024-09-12 06:13:27 +0200

    Create main.cu

---
+++ src/main.cu
@@ -0,0 +1,8 @@
+#include <stdio.h>
+__global__ void hello(){
+ printf("Hello from block: %u, thread: %u\n", blockIdx.x, threadIdx.x);
+}
+int main(){
+ hello<<<2, 2>>>();
+ cudaDeviceSynchronize();
+}
